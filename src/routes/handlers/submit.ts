84e336fdf00f40dd690162921d8457a03c44983f
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-02-28 14:31:21 +0300

    Move processSubmit function outside of handler

--- src/routes/handlers/submit.ts
+++ src/routes/handlers/submit.ts
@@ -1,4 +1,5 @@
 import Busboy from 'busboy';
+import { Request, Response } from 'express';
 import { createWriteStream } from 'fs';
 import { join } from 'path';
 import { Connection } from '../../lib/connection';
@@ -6,101 +7,101 @@ import { SUBMIT_TYPE, UPLOADS_DIR } from '../../lib/constants';
 import { Model, Task } from '../../lib/model';
 import { RequestHandler } from '../routes';
 
-export function handleSubmitTask(con: Connection): RequestHandler {
-    return (req, res) => {
-        const submit_type = req.query.type ? SUBMIT_TYPE[req.query.type.toUpperCase()] : null;
+const processSubmit = (body: Task, con: Connection, req: Request, res: Response) => {
+    const submit_type = req.query.type ? SUBMIT_TYPE[req.query.type.toUpperCase()] : null;
 
-        if (!submit_type) {
+    if (!submit_type) {
+        res.redirect('/');
+        return;
+    }
+
+    const { task, errors } = Model.validateTask(body);
+    const task_id = Number.parseInt(req.query.task_id, 10);
+    
+    if (errors) {
+        res.render('task_form', new Model([body], task_id, submit_type, errors));
+        return;
+    }
+
+    if (submit_type === SUBMIT_TYPE.EDIT) {
+        if (!task_id) {
             res.redirect('/');
             return;
         }
-
-        const processSubmit = (body: Task) => {
-            const { task, errors } = Model.validateTask(body);
-            const task_id = Number.parseInt(req.query.task_id, 10);
-            
-            if (errors) {
-                res.render('task_form', new Model([body], task_id, submit_type, errors));
-                return;
-            }
-
-            if (submit_type === SUBMIT_TYPE.EDIT) {
-                if (!task_id) {
-                    res.redirect('/');
-                    return;
+        con.query(
+            `UPDATE TASK SET
+                TASK_TEXT = $1,
+                TASK_STATUS = $2,
+                ESTIMATED_END_AT = $3,
+                FILE_ID = COALESCE($4, FILE_ID)
+            WHERE 
+                TASK_ID = ${task_id} AND (TASK_ID > 0)`,
+            [task.task_text, task.task_status, task.estimated_end_at, task.file_id],
+            (error, result) => {
+                if (error) {
+                    //TODO: add rendering with error messages
+                    throw error;
+                } else {
+                    con.query(
+                        `UPDATE TASK_FILE SET
+                            TASK_ID = $1
+                        WHERE
+                            FILE_ID = $2
+                        AND
+                            TASK_ID = -1`,
+                        [task_id, task.file_id],
+                        (error, result) => {
+                            if (error) {
+                                //TODO: add rendering with error messages
+                                throw error;
+                            } else {
+                                res.redirect('/');
+                            }
+                        }
+                    );
                 }
-                con.query(
-                    `UPDATE TASK SET
-                        TASK_TEXT = $1,
-                        TASK_STATUS = $2,
-                        ESTIMATED_END_AT = $3,
-                        FILE_ID = COALESCE($4, FILE_ID)
-                    WHERE 
-                        TASK_ID = ${task_id} AND (TASK_ID > 0)`,
-                    [task.task_text, task.task_status, task.estimated_end_at, task.file_id],
-                    (error, result) => {
-                        if (error) {
-                            //TODO: add rendering with error messages
-                            throw error;
-                        } else {
-                            con.query(
-                                `UPDATE TASK_FILE SET
-                                    TASK_ID = $1
-                                WHERE
-                                    FILE_ID = $2
-                                AND
-                                    TASK_ID = -1`,
-                                [task_id, task.file_id],
-                                (error, result) => {
-                                    if (error) {
-                                        //TODO: add rendering with error messages
-                                        throw error;
-                                    } else {
-                                        res.redirect('/');
-                                    }
-                                }
-                            );
-                        }
-                    }
-                );
-            } else {
-                con.query(
-                    `INSERT INTO TASK
-                        (TASK_TEXT, TASK_STATUS, CREATED_AT, ESTIMATED_END_AT, FILE_ID)
-                    VALUES
-                        ($1, $2, NOW(), $3, $4)
-                    RETURNING
-                        TASK_ID`,
-                    [task.task_text, task.task_status, task.estimated_end_at, task.file_id],
-                    (error, result) => {
-                        if (error || result.rows.length < 1) {
-                            //TODO: add rendering with error messages
-                            throw error;
-                        } else {
-                            const task_id = result.rows[0].task_id;
-                            con.query(
-                                `UPDATE TASK_FILE SET
-                                    TASK_ID = ${task_id}
-                                WHERE
-                                    FILE_ID = $1
-                                AND
-                                    TASK_ID = -1`,
-                                [task.file_id],
-                                (error, result) => {
-                                    if (error) {
-                                        //TODO: add rendering with error messages
-                                        throw error;
-                                    } else {
-                                        res.redirect('/');
-                                    }
-                                }
-                            );
-                        }
-                    }
-                );
             }
-        }
+        );
+    } else {
+        con.query(
+            `INSERT INTO TASK
+                (TASK_TEXT, TASK_STATUS, CREATED_AT, ESTIMATED_END_AT, FILE_ID)
+            VALUES
+                ($1, $2, NOW(), $3, $4)
+            RETURNING
+                TASK_ID`,
+            [task.task_text, task.task_status, task.estimated_end_at, task.file_id],
+            (error, result) => {
+                if (error || result.rows.length < 1) {
+                    //TODO: add rendering with error messages
+                    throw error;
+                } else {
+                    const task_id = result.rows[0].task_id;
+                    con.query(
+                        `UPDATE TASK_FILE SET
+                            TASK_ID = ${task_id}
+                        WHERE
+                            FILE_ID = $1
+                        AND
+                            TASK_ID = -1`,
+                        [task.file_id],
+                        (error, result) => {
+                            if (error) {
+                                //TODO: add rendering with error messages
+                                throw error;
+                            } else {
+                                res.redirect('/');
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    }
+}
 
+export function handleSubmitTask(con: Connection): RequestHandler {
+    return (req, res) => {
         const fields: Array<Promise<any>> = [];
 
         const busboy = new Busboy({
@@ -154,7 +155,7 @@ export function handleSubmitTask(con: Connection): RequestHandler {
                         ...field
                     };
                 });
-                processSubmit(body);
+                processSubmit(body, con, req, res);
             }).catch((reason) => {
                 console.log(reason);
             });

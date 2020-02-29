ab284ac16215553530a312c0dd4b0a4967b685d7
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-02-29 21:58:05 +0300

    Remove unnecessary operations & code style unification

--- src/routes/handlers/download.ts
+++ src/routes/handlers/download.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'fs';
-import { basename, join, normalize } from 'path';
+import { join } from 'path';
 import { Connection } from '../../lib/connection';
 import { UPLOADS_DIR } from '../../lib/constants';
 import { RequestHandler } from '../routes';
@@ -8,17 +8,17 @@ export function handleDownload(con: Connection): RequestHandler {
     return (req, res) => {
         const file_id = Number.parseInt(req.query.file_id);
         if (file_id) {
-            const filename = join(process.cwd(), UPLOADS_DIR, basename(normalize(String(file_id))));
+            const filename = join(process.cwd(), UPLOADS_DIR, String(file_id));
             if (existsSync(filename)) {
                 con.query(
                     `SELECT FILE_NAME FROM TASK_FILE
                     WHERE
                         FILE_ID = ${file_id}`, 
                     (error, result) => {
-                        if (!error && result.rows.length >= 1) {
-                            res.download(filename, result.rows[0].file_name);
-                        } else {
+                        if (error || result.rows.length < 1) {
                             res.status(404).end();
+                        } else {
+                            res.download(filename, result.rows[0].file_name);
                         }
                     }
                 );

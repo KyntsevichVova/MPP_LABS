8db32f6f627efbe8146f7dfc0cd41d3c31c6120f
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-03-04 17:11:05 +0300

    Add Routes

--- app/frontend/src/components/App/App.tsx
+++ app/frontend/src/components/App/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div></div>
-  );
+  	return (
+    	<HashRouter>
+			<Switch>
+				<Route exact path='/'>
+					
+				</Route>
+				<Route exact path='/add'>
+
+				</Route>
+				<Route exact path='/edit/:task_id'>
+					
+				</Route>
+			</Switch>
+    	</HashRouter>
+  	);
 }
 
 export default App;

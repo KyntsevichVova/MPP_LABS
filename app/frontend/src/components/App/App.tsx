48c23915fdceb74c99683a1cc0cf3f8c27e6d321
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-03-04 17:19:34 +0300

    Add basic components for pages

--- app/frontend/src/components/App/App.tsx
+++ app/frontend/src/components/App/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import AddPage from '../pages/Add/Add';
+import EditPage from '../pages/Edit/Edit';
+import HomePage from '../pages/Home/Home';
 
 function App() {
   	return (
     	<HashRouter>
 			<Switch>
 				<Route exact path='/'>
-					
+					<HomePage />
 				</Route>
 				<Route exact path='/add'>
-
+					<AddPage />
 				</Route>
 				<Route exact path='/edit/:task_id'>
-					
+					<EditPage />
 				</Route>
 			</Switch>
     	</HashRouter>
---
+++ app/frontend/src/components/pages/Edit/Edit.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function EditPage() {
+    return (
+        
+    );
+}
+
+export default EditPage;

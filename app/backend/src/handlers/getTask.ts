6e62899fdd1e65c6694bb6aca809a5c47cb5cc15
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-03-13 14:24:11 +0300

    Rework getTask flow

--- app/backend/src/handlers/getTask.ts
+++ app/backend/src/handlers/getTask.ts
@@ -22,21 +22,19 @@ export function getTask(con: Connection): RequestHandler {
                     TASK_ID > 0
                 AND
                     CREATED_BY = $2`,
-                [task_id, payload.user_id],
-                (error, result) => {
-                    if (error) {
-                        throw Exception.DatabaseError(error);
-                    }
-                    if (result.rows.length < 1) {
-                        throw Exception.EntityNotFound();
-                    } else {
-                        res
-                            .status(HttpStatus.OK)
-                            .send(new Model(result.rows.map(Model.createTask)))
-                            .end();
-                    }
+                [task_id, payload.user_id]
+            ).then((result) => {
+                if (result.rows.length < 1) {
+                    throw Exception.EntityNotFound();
                 }
-            );
+
+                res
+                    .status(HttpStatus.OK)
+                    .send(new Model(result.rows.map(Model.createTask)))
+                    .end();
+            }).catch((error) => {
+                throw Exception.DatabaseError(error);
+            });
         }).catch(next);
     }
 }

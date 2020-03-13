ef07dcc4af4132d48dbf79301d6cd762b3d23fd2
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-03-13 14:22:05 +0300

    Rework getFile flow

--- app/backend/src/handlers/getFile.ts
+++ app/backend/src/handlers/getFile.ts
@@ -10,38 +10,37 @@ export function getFile(con: Connection): RequestHandler {
         Promise.resolve().then(() => {
             const file_id = Number.parseInt(req.params.file_id, 10);
 
-            const payload = req.payload;
-        
-            if (file_id) {
-                const filename = join(process.cwd(), UPLOADS_DIR, String(file_id));
-                if (existsSync(filename)) {
-                    con.query(
-                        `SELECT
-                            FILE_NAME
-                        FROM
-                            TASK_FILE
-                        WHERE
-                            FILE_ID = ${file_id}
-                        AND
-                            CREATED_BY = $1`,
-                        [payload.user_id], 
-                        (error, result) => {
-                            if (error) {
-                                throw Exception.DatabaseError(error);
-                            }
-                            if (result.rows.length < 1) {
-                                throw Exception.FileNotFound();
-                            } else {
-                                res.download(filename, result.rows[0].file_name);
-                            }
-                        }
-                    );
-                } else {
-                    throw Exception.FileNotFound();
-                }
-            } else {
+            if (!file_id) {
                 throw Exception.BadRequest();
             }
+        
+            const filename = join(process.cwd(), UPLOADS_DIR, String(file_id));
+
+            if (!existsSync(filename)) {
+                throw Exception.FileNotFound();
+            }
+
+            const payload = req.payload;
+
+            con.query(
+                `SELECT
+                    FILE_NAME
+                FROM
+                    TASK_FILE
+                WHERE
+                    FILE_ID = ${file_id}
+                AND
+                    CREATED_BY = $1`,
+                [payload.user_id]
+            ).then((result) => {
+                if (result.rows.length < 1) {
+                    throw Exception.FileNotFound();
+                }
+
+                res.download(filename, result.rows[0].file_name);
+            }).catch((error) => {
+                throw Exception.DatabaseError(error);
+            });
         }).catch(next);
     }
 }
